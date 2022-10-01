76a3922eff4da83c041e0617c79d39e4ffe9c1b1
Armageddon1 <97532780+Armageddon1@users.noreply.github.com>
2022-10-01 21:56:57 +0800

    Add files via upload
    初始配置

---
+++ src/test/java/com/rabbittakeout/RabbitTakeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbittakeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitTakeOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
